278b6d8cdd0b3c10b379794684ab5a61c107fcd2
chafroudtarek <chafroudtarek6@gmail.com>
2025-01-28 16:11:24 +0100

    implement toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import 'react-toastify/dist/ReactToastify.css';
+
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./queryClient";
+import { ToastContainer } from "react-toastify";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ToastContainer />
+
     </QueryClientProvider>
   </StrictMode>
 );
