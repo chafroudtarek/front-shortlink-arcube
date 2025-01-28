442c3dbad013b4fc020d9e530008a04df80da054
chafroudtarek <chafroudtarek6@gmail.com>
2025-01-28 14:41:30 +0100

    setup tailwind & eslint & reactquery

---
+++ src/utils/shared-base-http.ts
@@ -0,0 +1,85 @@
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import axios from "axios";
+
+export interface Params {
+  [key: string]:
+    | boolean
+    | string
+    | string[]
+    | number
+    | number[]
+    | {
+        [key: string]: string[];
+      }
+    | undefined;
+}
+
+export interface HttpParamsType<T = unknown> {
+  queryParams?: Params;
+  body?: T;
+  config?: AxiosRequestConfig;
+  headers?: Record<string, string>;
+  isUpload?: boolean;
+}
+
+class HttpClient {
+  private instance: AxiosInstance;
+
+  constructor() {
+    this.instance = axios.create({
+      baseURL: process.env.NEXT_PUBLIC_API_URL,
+      withCredentials: true,
+    });
+
+    this.setupInterceptors();
+  }
+
+  private setupInterceptors(): void {
+    this.instance.interceptors.request.use(
+      async (config) => {
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+
+    this.instance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const prevRequest = error?.config;
+
+        if (error?.response?.status === 401 && !prevRequest?.sent) {
+          prevRequest.sent = true;
+        }
+
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  async post<T>(url: string, paramsData?: HttpParamsType<T>): Promise<AxiosResponse<T>> {
+    const { body = {}, config = {}, headers = {} } = paramsData || {};
+
+    return this.instance.post<T>(url, body, {
+      ...config,
+      headers: {
+        ...headers,
+      },
+    });
+  }
+
+  async get<T>(url: string, paramsData?: HttpParamsType<T>): Promise<AxiosResponse<T>> {
+    const { queryParams = {}, config = {}, headers = {} } = paramsData || {};
+
+    return this.instance.get<T>(url, {
+      ...config,
+      params: queryParams,
+      headers: {
+        ...headers,
+      },
+    });
+  }
+}
+
+export const httpClient = new HttpClient();
